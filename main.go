a97610055a2a068488b9ecdd5444d5a42963de71
Vincent Larrat <vincentla@theodo.fr>
2019-10-17 16:00:29 +0200

    Un langage typé statiquement (variables)

--- main.go
+++ main.go
@@ -11,7 +11,16 @@ func main() {
 }
 
 func teachToTheodoers(w http.ResponseWriter, r *http.Request) {
-	_, _ = fmt.Fprint(w, "Hello Theodoers!")
+	var welcomeWord string
+	var firstName, lastName string = "John", "Doe"
+	var courseNumber int = 1
+
+	welcomeWord = "Hello"
+	language := "GO"
+
+	_, _ = fmt.Fprintf(w, "%s %s %s :)\n", welcomeWord, firstName, lastName)
+	_, _ = fmt.Fprintf(w, "Welcome to %s course #%v!\n", language, courseNumber)
 }
 
-// Browser output : Hello Theodoers!
+// Browser output : Hello John Doe :)
+//					Welcome to GO course #1!
